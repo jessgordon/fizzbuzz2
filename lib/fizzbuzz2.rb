6afa131d3df4495f4bf336522793f30010bebdd8
Jessica Gordon <ketchesj@gmail.com>
2021-11-12 12:32:35 +0000

    rspec passed for multiples of 3 and 5 combined

--- lib/fizzbuzz2.rb
+++ lib/fizzbuzz2.rb
@@ -1,8 +1,8 @@
 def fizzbuzz(n)
   answer = ""
-  
+
   answer = 'fizz' if n % 3 == 0
-  answer = 'buzz' if n % 5 == 0
+  answer += 'buzz' if n % 5 == 0
 
   answer
 end
--- spec/fizzbuzz2_spec.rb
+++ spec/fizzbuzz2_spec.rb
@@ -7,4 +7,7 @@ describe 'fizzbuzz2' do
   it 'returns "buzz" when passed a multiple of 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+  it 'returns "fizzbuzz" when passed a multiple of 5 and a multiple of 3' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end 
 end
