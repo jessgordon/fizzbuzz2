055e6905ff0e8a03f11675a6923da3177a9a03c4
Sabrina <sabrinalord@msn.com>
2021-11-12 13:00:44 +0000

    refactored to ternary

--- lib/fizzbuzz2.rb
+++ lib/fizzbuzz2.rb
@@ -1,9 +1,6 @@
 def fizzbuzz(n)
   answer = (n % 3 == 0 ? 'fizz' : '') + (n % 5 == 0 ? 'buzz' : '')
-  answer = n if answer.empty?
-  answer
+  answer.empty? ? n : answer
+  
 end
 
-#def fizzbuzz(n)
-#  [('Fizz' if x % 3 == 0), ('Buzz' if x % 5 == 0)].compact.inject(:+) || x
-#end
